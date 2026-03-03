6dbba9a87cb887cb5ceb84207fe5b5a19d31fada
yang-ze-kang <603822317@qq.com>
2026-03-03 15:40:23 +0800

    260303

--- swclib/data/swc.py
+++ swclib/data/swc.py
@@ -3,6 +3,7 @@ from scipy.spatial import cKDTree
 import os
 from copy import deepcopy
 import math
+from datetime import datetime, timezone
 
 from swclib.data.swc_tree import SwcTree
 from swclib.data.swc_node import nodes2coords
@@ -144,7 +145,7 @@ class Swc(object):
     def __init__(self, file_name=None):
         self.nodes = {}
         self.edges = []
-        self.bound_box = [0, 0, 0, 0, 0, 0]  # x0,y0,z0,x1,y1,z1
+        self.bound_box = [np.inf, np.inf, np.inf, 0, 0, 0]  # x0,y0,z0,x1,y1,z1
         self.file_name = file_name
         if file_name is not None:
             self.open(file_name)
@@ -187,6 +188,16 @@ class Swc(object):
         return np.array(
             [[node["x"], node["y"], node["z"]] for node in self.nodes.values()]
         )
+    
+    def get_roots(self, return_coords=True):
+        roots = []
+        for nid, node in self.nodes.items():
+            pid = int(node["parent"])
+            if pid == -1:
+                roots.append(nid)
+        if return_coords:
+            return [[self.nodes[nid]["x"], self.nodes[nid]["y"], self.nodes[nid]["z"]] for nid in roots]
+        return roots
 
     def rescale(self, scale):
         """
@@ -205,78 +216,6 @@ class Swc(object):
             self.bound_box[5] * scale[2],
         ]
         return self
-
-    # def resample(self, min_distance=2.0):
-    #     """
-    #     Densify edges so that the distance between adjacent nodes (node-parent chain)
-    #     is <= min_distance by inserting intermediate nodes on long edges.
-    #     """
-    #     if len(self.nodes) == 0:
-    #         return self
-
-    #     next_id = max(self.nodes.keys()) + 1
-
-    #     new_nodes = {}
-    #     new_edges = []
-    #     for nid, node in self.nodes.items():
-    #         new_nodes[nid] = dict(node)
-
-    #     for nid in sorted(self.nodes.keys()):
-    #         node = new_nodes[nid]
-    #         pid = int(node["parent"])
-
-    #         if pid == -1 or pid not in new_nodes:
-    #             new_edges.append((nid, pid))
-    #             continue
-
-    #         parent = new_nodes[pid]
-    #         p = np.array([parent["x"], parent["y"], parent["z"]], dtype=float)
-    #         c = np.array([node["x"], node["y"], node["z"]], dtype=float)
-    #         v = c - p
-    #         dist = float(np.linalg.norm(v))
-    #         if dist <= min_distance or dist == 0.0:
-    #             new_edges.append((nid, pid))
-    #             continue
-
-    #         m = int(np.ceil(dist / float(min_distance)))
-    #         num_insert = max(0, m - 1)
-    #         chain_ids = []
-    #         for k in range(1, num_insert + 1):
-    #             t = k / float(m)
-    #             xyz = p + t * v
-    #             r = float(parent.get("radius", 1.0)) + t * (float(node.get("radius", 1.0)) - float(parent.get("radius", 1.0)))
-
-    #             new_nodes[next_id] = {
-    #                 "id": next_id,
-    #                 "type": 5, # 5 indicates an inserted node
-    #                 "x": float(xyz[0]),
-    #                 "y": float(xyz[1]),
-    #                 "z": float(xyz[2]),
-    #                 "radius": r,
-    #                 "parent": None,  # fill later
-    #             }
-    #             chain_ids.append(next_id)
-    #             next_id += 1
-
-    #         # pid -> chain[0] -> ... -> chain[-1] -> nid
-    #         if chain_ids:
-    #             new_nodes[chain_ids[0]]["parent"] = pid
-    #             new_edges.append((chain_ids[0], pid))
-    #             for a, b in zip(chain_ids[1:], chain_ids[:-1]):
-    #                 new_nodes[a]["parent"] = b
-    #                 new_edges.append((a, b))
-    #             last = chain_ids[-1]
-    #             node["parent"] = last
-    #             new_edges.append((nid, last))
-    #         else:
-    #             new_edges.append((nid, pid))
-    #     self.nodes = new_nodes
-    #     self.edges = new_edges
-    #     xs = [n["x"] for n in self.nodes.values()]
-    #     ys = [n["y"] for n in self.nodes.values()]
-    #     zs = [n["z"] for n in self.nodes.values()]
-    #     self.bound_box = [min(xs), min(ys), min(zs), max(xs), max(ys), max(zs)]
-    #     return self
 
     def resample(self, min_distance=2.0, in_place=True):
         """
@@ -746,7 +685,9 @@ class Swc(object):
 
             with open(out_path, "w", encoding="utf-8") as f:
                 if write_header:
+                    ts = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
                     f.write("# Exported by Swc.save_to_swc\n")
+                    f.write(f"# Export time (UTC): {ts}\n")
                     f.write("# id type x y z radius parent\n")
 
                 for nid in node_ids:
